6ca697603a0768fa9b445881e5e7ef4e36be58eb
Martin Grotzke <martin.grotzke@googlemail.com>
2013-10-20 23:13:20 +0200

    Makes tests green

---
+++ test/ShopsControllerSpec.scala
@@ -0,0 +1,80 @@
+import org.specs2.mutable._
+import org.specs2.runner._
+import org.junit.runner._
+import play.api.test._
+import play.api.test.Helpers._
+import net.fwbrasil.activate.test._
+
+@RunWith(classOf[JUnitRunner])
+class ShopsControllerSpec extends Specification with ActivateTest {
+
+  override def strategy: Strategy = recreateDatabaseStrategy
+  override def context(app: play.api.Application) = models.activate.shopPersistenceContext
+
+  "ShopsController" should {
+    
+    "redirect to the shop list on /" inActivate {
+
+      val result = controllers.ShopsController.index(FakeRequest())
+
+      status(result) must equalTo(SEE_OTHER)
+      redirectLocation(result) must beSome.which(_ == "/shops")
+
+    }
+
+    "list shops on the the first page" inActivate {
+
+      val result = controllers.ShopsController.list(0, 2, "")(FakeRequest())
+
+      status(result) must equalTo(OK)
+      contentAsString(result) must contain("One shop found")
+
+    }
+    
+    "filter shop by name" inActivate {
+
+      val result = controllers.ShopsController.list(0, 2, "Pauli")(FakeRequest())
+
+      status(result) must equalTo(OK)
+      contentAsString(result) must contain("One shop found")
+
+    }
+    
+    "create new shop" inActivate {
+
+      val badResult = controllers.ShopsController.save(FakeRequest())
+      status(badResult) must equalTo(BAD_REQUEST)
+
+      val missingFields = controllers.ShopsController.save(
+        FakeRequest().withFormUrlEncodedBody("name" -> "FooBar")
+      )
+      status(missingFields) must equalTo(BAD_REQUEST)
+      contentAsString(missingFields) must contain("""<input type="text" id="name" name="name" value="FooBar" >""")
+
+      val result = controllers.ShopsController.save(
+        FakeRequest().withFormUrlEncodedBody(
+          "name" -> "FooBar",
+          "url" -> "url",
+          "active" -> "false",
+          "queryUrlTemplate" -> "queryUrlTemplate",
+          "imageUrlBase" -> "imageUrlBase",
+          "itemXPath" -> "itemXPath",
+          "nameXPath" -> "nameXPath",
+          "priceXPath" -> "priceXPath",
+          "imageUrlXPath" -> "imageUrlXPath",
+          "detailsUrlXPath" -> "detailsUrlXPath"))
+
+      status(result) must equalTo(SEE_OTHER)
+      redirectLocation(result) must beSome.which(_ == "/shops")
+      flash(result).get("success") must beSome.which(_ == "Shop FooBar has been created")
+
+      val list = controllers.ShopsController.list(0, 2, "FooBar")(FakeRequest())
+
+      status(list) must equalTo(OK)
+      contentAsString(list) must contain("One shop found")
+
+    }
+    
+  }
+  
+}
---
+++ test/ShopsIntegrationSpec.scala
@@ -0,0 +1,66 @@
+import org.specs2.mutable._
+import play.api.test._
+import play.api.test.Helpers._
+import org.fluentlenium.core.filter.FilterConstructor._
+import net.fwbrasil.activate.test._
+
+class ShopsIntegrationSpec extends Specification with ActivateTest {
+
+  // reinitializeContext
+  
+  override def strategy: Strategy = recreateDatabaseStrategy
+  override def context(app: play.api.Application) = models.activate.shopPersistenceContext
+
+  "Application" should {
+
+    "work from within a browser" inBrowserWithActivate { browser =>
+
+      browser.goTo("http://localhost:3333/")
+
+      browser.$("header h1").first.getText must equalTo("Play sample application — Shopping search buddy")
+      browser.$("section h1").first.getText must equalTo("One shop found")
+
+      browser.$("#pagination li.current").first.getText must equalTo("Displaying 1 to 1 of 1")
+
+//      browser.$("#pagination li.next a").click()
+//
+//      browser.$("#pagination li.current").first.getText must equalTo("Displaying 11 to 20 of 565")
+      browser.$("#searchbox").text("Pauli")
+      browser.$("#searchsubmit").click()
+
+      browser.$("section h1").first.getText must equalTo("One shop found")
+      browser.$("a", withText("FC St. Pauli")).click()
+
+      browser.$("section h1").first.getText must equalTo("Edit shop")
+
+      browser.$("#url").text("")
+      browser.$("input.primary").click()
+
+      browser.$("div.error").size must equalTo(1)
+      browser.$("div.error label").first.getText must equalTo("URL")
+
+      browser.$("#url").text("http://example.com")
+      browser.$("input.primary").click()
+
+      browser.$("section h1").first.getText must equalTo("One shop found")
+      browser.$(".alert-message").first.getText must equalTo("Done! Shop FC St. Pauli has been updated")
+
+      browser.$("#searchbox").text("Pauli")
+      browser.$("#searchsubmit").click()
+
+      browser.$("a", withText("FC St. Pauli")).click()
+      browser.$("input.danger").click()
+
+      browser.$("section h1").first.getText must equalTo("No shops found")
+      browser.$(".alert-message").first.getText must equalTo("Done! Shop has been deleted")
+
+      browser.$("#searchbox").text("Pauli")
+      browser.$("#searchsubmit").click()
+
+      browser.$("section h1").first.getText must equalTo("No shops found")
+
+    }
+
+  }
+
+}
